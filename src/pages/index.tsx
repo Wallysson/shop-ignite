a750b65d33843bf3aea74ae1012529ff9a54e124
Wallysson <wlc.couto@gmail.com>
2022-10-01 14:38:09 -0300

    create: Navigation via Link (Next), Page Layout of Product, SSG with dinamic parameter and Fallback

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,14 @@ import 'keen-slider/keen-slider.min.css'
 import { GetStaticProps } from 'next'
 import { stripe } from '../lib/stripe'
 import Stripe from 'stripe'
+import Link from 'next/link'
 
 interface HomeProps {
   products: {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -30,13 +31,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef}>
       {products.map(product => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link key={product.id} href={`/product/${product.id}`}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
