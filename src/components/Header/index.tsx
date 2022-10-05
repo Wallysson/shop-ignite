418bddef36110b3acf76862fb3c2b9ec4ec94818
Wallysson <wlc.couto@gmail.com>
2022-10-05 09:03:15 -0300

    update: removing cart button in page success

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,19 @@ import logoImg from '../../assets/logo.svg'
 import Image from 'next/future/image'
 import Link from 'next/link'
 import { Cart } from '../Cart'
+import { useRouter } from 'next/router'
 
 export function Header() {
+  const { pathname } = useRouter()
+
+  const showCartButton = pathname !== '/success'
+
   return (
     <HeaderContainer>
       <Link href="/">
         <Image src={logoImg} alt="" />
       </Link>
-      <Cart />
+      {showCartButton && <Cart />}
     </HeaderContainer>
   )
 }
